a8b520710731d7f9b7595fdebd50d0ceee31a690
TienNguyen79 <Tientech2002@gmail.com>
2024-02-29 11:31:55 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React hihi
         </a>
+        <h1 className="text-red-500">okok</h1>
       </header>
     </div>
   );
